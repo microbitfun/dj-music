e36c875b134850a03289164bc5f29c2843f239b3
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-01 17:10:45 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let sound = 0
+basic.forever(function () {
+    sound = pins.analogReadPin(AnalogPin.P1)
+    sound = sound * 2
+    if (sound < 300) {
+        music.playTone(sound, 100)
+    }
+    if (sound > 300) {
+        music.playTone(0, 0)
+    }
+})
